e74f54f1283ba339b3bfa8306e209588965b82fd
angelip2303 <angel.iglesias.prestamo@gmail.com>
2023-05-14 15:39:12 +0200

    including ci/cd

--- src/pschema.rs
+++ src/pschema.rs
@@ -1,4 +1,3 @@
-use polars::error::ErrString;
 use crate::shape::Shape::{WShape, WShapeComposite, WShapeLiteral, WShapeRef};
 use crate::shape::{Shape, ShapeIterator, Validate};
 
@@ -16,27 +15,33 @@ impl PSchema {
     }
 
     pub fn validate(&self, graph: GraphFrame) -> Result<DataFrame, PolarsError> {
-        // First, we check if the graph is empty or not. If the graph is empty, we return an error.
-        if graph.vertices.is_empty() || graph.edges.is_empty() {
-            return Err(PolarsError::NoData(ErrString::from("The graph is empty")));
-        }
-        // Then, we check if the graph has the required columns. If the graph does not have the
+        // First, we check if the graph has the required columns. If the graph does not have the
         // required columns, we return an error. The required columns are:
         //  - src: the source vertex of the edge
         //  - dst: the destination vertex of the edge
         //  - property_id: the property id of the edge
         //  - dtype: the data type of the property
+        // Then, for each column we check if the column is empty. If the column is empty, we return
+        // an error.
         if graph.edges.schema().get_field("src").is_none() {
-            return Err(PolarsError::SchemaFieldNotFound(ErrString::from("src")));
+            return Err(PolarsError::SchemaFieldNotFound("src".into()));
+        } else if graph.edges.column("src").unwrap().len() == 0 {
+            return Err(PolarsError::NoData("src".into()));
         }
         if graph.edges.schema().get_field("dst").is_none() {
-            return Err(PolarsError::SchemaFieldNotFound(ErrString::from("dst")));
+            return Err(PolarsError::SchemaFieldNotFound("dst".into()));
+        } else if graph.edges.column("dst").unwrap().len() == 0 {
+            return Err(PolarsError::NoData("dst".into()));
         }
         if graph.edges.schema().get_field("property_id").is_none() {
-            return Err(PolarsError::SchemaFieldNotFound(ErrString::from("property_id")));
+            return Err(PolarsError::SchemaFieldNotFound("property_id".into()));
+        } else if graph.edges.column("property_id").unwrap().len() == 0 {
+            return Err(PolarsError::NoData("property_id".into()));
         }
         if graph.edges.schema().get_field("dtype").is_none() {
-            return Err(PolarsError::SchemaFieldNotFound(ErrString::from("dtype")));
+            return Err(PolarsError::SchemaFieldNotFound("dtype".into()));
+        } else if graph.edges.column("dtype").unwrap().len() == 0 {
+            return Err(PolarsError::NoData("dtype".into()));
         }
         // First, we need to define the maximum number of iterations that will be executed by the
         // algorithm. In this case, we will execute the algorithm until the tree converges, so we
@@ -50,7 +55,8 @@ impl PSchema {
                             // Then, we can define the algorithm that will be executed on the graph. The algorithm
                             // will be executed in parallel on all vertices of the graph.
         let pregel = PregelBuilder::new(graph)
-            .max_iterations(if max_iterations > 1 { // This is a Theorem :D
+            .max_iterations(if max_iterations > 1 {
+                // This is a Theorem :D
                 max_iterations - 1
             } else {
                 1
@@ -146,7 +152,6 @@ mod tests {
         BirthDate,
         London,
         AwardReceived,
-        TogetherWith,
         UnitedKingdom,
     }
 
@@ -166,7 +171,6 @@ mod tests {
                 BirthDate => Id::from("P569"),
                 London => Id::from("Q84"),
                 AwardReceived => Id::from("P166"),
-                TogetherWith => Id::from("P170"),
                 UnitedKingdom => Id::from("Q145"),
             };
             u64::from(id)
@@ -283,47 +287,89 @@ mod tests {
         let schema = paper_schema();
 
         match PSchema::new(schema).validate(graph) {
-            Ok(result) => {
-                println!("{}", result);
-                Ok(())
-            }
-            Err(error) => Err(error.to_string()),
-        }
-    }
-
-    #[test]
-    fn empty_graph() -> Result<(), String> {
-        let graph = match paper_graph() {
-            Ok(graph) => graph,
-            Err(error) => return Err(error),
-        };
-
-        let schema = paper_schema();
-
-        match PSchema::new(schema).validate(graph) {
-            Ok(result) => {
-                println!("{}", result);
-                Ok(())
-            }
+            Ok(_) => Ok(()),
             Err(error) => Err(error.to_string()),
         }
     }
 
     #[test]
     fn invalid_graph() -> Result<(), String> {
-        let graph = match paper_graph() {
-            Ok(graph) => graph,
-            Err(error) => return Err(error),
+        let edges = match df![
+            Column::Src.as_ref() => [
+                TimBernersLee,
+                TimBernersLee,
+                London,
+                TimBernersLee,
+                TimBernersLee,
+                Award,
+                VintCerf,
+                CERN,
+                TimBernersLee,
+            ]
+            .iter()
+            .map(TestEntity::id)
+            .collect::<Vec<_>>(),
+            Column::Dst.as_ref() => [
+                Human,
+                London,
+                UnitedKingdom,
+                CERN,
+                Award,
+                Spain,
+                Human,
+                Award,
+                TimBernersLee,
+            ]
+            .iter()
+            .map(TestEntity::id)
+            .collect::<Vec<_>>(),
+        ] {
+            Ok(edges) => edges,
+            Err(_) => return Err(String::from("Error creating the edges DataFrame")),
         };
 
-        let schema = Shape::WShape(WShape::new("IsHuman", InstanceOf.id(), Human.id()));
+        let graph = match GraphFrame::from_edges(edges) {
+            Ok(graph) => graph,
+            Err(_) => return Err(String::from("Error creating the GraphFrame from edges")),
+        };
+
+        let schema = simple_schema();
 
         match PSchema::new(schema).validate(graph) {
-            Ok(result) => {
-                println!("{}", result);
-                Ok(())
-            }
-            Err(error) => Err(error.to_string()),
+            Ok(_) => Err(String::from("An error should have occurred")),
+            Err(_) => Ok(()),
+        }
+    }
+
+    #[test]
+    fn empty_graph() -> Result<(), String> {
+        let vertices = match df![
+            Column::Id.as_ref() => Series::default(),
+        ] {
+            Ok(vertices) => vertices,
+            Err(_) => return Err(String::from("Error creating the vertices DataFrame")),
+        };
+
+        let edges = match df![
+            Column::Src.as_ref() => Series::default(),
+            Column::Custom("property_id").as_ref() => Series::default(),
+            Column::Dst.as_ref() => Series::default(),
+            Column::Custom("dtype").as_ref() => Series::default(),
+        ] {
+            Ok(edges) => edges,
+            Err(_) => return Err(String::from("Error creating the edges DataFrame")),
+        };
+
+        let graph = match GraphFrame::new(vertices, edges) {
+            Ok(graph) => graph,
+            Err(_) => return Err(String::from("Error creating the GraphFrame from edges")),
+        };
+
+        let schema = simple_schema();
+
+        match PSchema::new(schema).validate(graph) {
+            Ok(_) => Err(String::from("An error should have occurred")),
+            Err(_) => Ok(()),
         }
     }
 }
